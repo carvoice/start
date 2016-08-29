d50a3b41de4de6595072acf5db808cb3e3eef2a6
carvoice <carvoice@mail.ru>
2016-08-29 20:19:01 +0300

    Пиздец

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,25 +10,59 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
+            int x = 1;
+            Func1(x);
+            Console.WriteLine("Call Func1. x = " + x);
 
-            Draw(x1, y1, sym1);
+            x = 1;
+            Func2(x);
+            Console.WriteLine("Call Func.2. x = " + x);
 
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
+            x = 1;
+            Func3(x);
+            Console.WriteLine("Call Func.3/ x =" + x);
 
-            Draw(x2, y2, sym2);
+            Point p1 = new Point(1, 3, '*');
+            Move(p1, 10, 10);
+            Console.WriteLine("Call Move. p1.x =" + p1.x + "p1.y =" + p1.y);
+
+            Point p2 = new Point(4, 5, '#');
+            p1 = p2;
+            p2.x = 8;
+            p2.y = 8;
+            Console.WriteLine("p = p2. p1.x =" + p1.x + ", p1.y = " + p1.y + "; p2.x = " + p2.x + ", p2.y = " + p2.y);
+
+            p1 = new Point(1, 3, '*');
+            Update(p1);
+            Console.WriteLine("Call Move. p1.x =" + p1.x + ", p2.y = " + p2.y);
 
             Console.ReadLine();
+
         }
 
-        static void Draw(int x, int y, char sym)
+        public static void Func1(int value)
+
+        public static void Func2(int value)
         {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
+            value = value + 1;
+        }
+
+        public static void Func3(int x)
+        {
+            x = x + 1;
+        }
+
+        public static void Move(Point p; int dx, int dy)
+        {
+            p.x = p.x + dx;
+            p.y = p.y + dy;
+        }
+
+        public static void Main()
+        {
+        p = new Point();
+        }
+
         }
   
         
