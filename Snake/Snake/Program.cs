299d6403dde5bfe0cd524249f99b85c1937a5f20
carvoice <carvoice@mail.ru>
2016-08-30 16:45:35 +0300

    Создан класс Змейка

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -22,12 +22,11 @@ namespace Snake
             rightline.Draw();
 
             Point p = new Point(4, 5, '*');
-            p.Draw();
-                                         
-            Console.ReadLine();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
 
-        }
-                 
-}
+            Console.ReadLine();
+        }           
     }
+}
 
