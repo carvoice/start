6ac9be1eafd20e380e7e0baeebb4942b77c46c92
carvoice <carvoice@mail.ru>
2016-08-30 15:26:11 +0300

    Создан класс Горизонтальная линия и вертикальная линия

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,29 +13,11 @@ namespace Snake
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
             
-
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-            
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> plist = new List<Point>();
-            plist.Add(p1);
-            plist.Add(p2);
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Draw();
                                          
             Console.ReadLine();
 
