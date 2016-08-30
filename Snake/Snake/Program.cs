6a45bc5638f8baf61919ec6a01d46a9d5fb83d7a
carvoice <carvoice@mail.ru>
2016-08-30 16:04:31 +0300

    Создан базовый класс Фигура

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,17 +10,19 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
-            
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            Console.SetBufferSize(80, 25);
 
-            HorizontalLine line = new HorizontalLine(0, 6, 8, '+');
-            line.Draw();
+            HorizontalLine upline = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftline = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightline = new VerticalLine(0, 24, 78, '+');
+            upline.Draw();
+            downline.Draw();
+            leftline.Draw();
+            rightline.Draw();
 
-            VerticalLine line1 = new VerticalLine(8, 13, 0, '+');
-            line1.Draw();
+            Point p = new Point(4, 5, '*');
+            p.Draw();
                                          
             Console.ReadLine();
 
